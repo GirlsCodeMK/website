f44c014e7e282485829b16b6ccd786def31e884d
Georgina Hodgkinson <gtl90@hotmail.com>
2019-11-04 21:43:32 +0000

    add engines for build

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import logo from "../public/logo192.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Girls Code MK</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
