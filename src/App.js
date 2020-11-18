37bc064c548dd907b43b512711bf00bc267f95cf
Georgina Hodgkinson <gtl90@hotmail.com>
2020-11-18 00:47:32 +0000

    Added covid paragraph about face to face meetings

--- src/App.js
+++ src/App.js
@@ -15,30 +15,8 @@ function App() {
       <a href="https://girlscodemk.herokuapp.com/">
         <img src={Logo} alt="Girls Code MK Logo" />
       </a>
-      <div id="Menu">
-        <ul>
-          <li>
-            <a href="https://girlscodemk.herokuapp.com/">Home</a>
-          </li>
-          <li>
-            <a href="https://girlscodemk.herokuapp.com/about">About</a>
-          </li>
-          <li>
-            <a href="https://girlscodemk.herokuapp.com/blog">Blog</a>
-          </li>
-          <li>
-            <a href="https://girlscodemk.herokuapp.com/codeofconduct">
-              Code of Conduct
-            </a>
-          </li>
-          <li>
-            <a href="https://girlscodemk.herokuapp.com/contactus">Contact Us</a>
-          </li>
-        </ul>
-      </div>
       <header className="App-header">
-        <img src={Mansion} alt="Bletchley Park Mansion" />
-        <p>Girls Code MK</p>
+        Girls Code MK
         <p>
           We support girls and women in their coding journey by offering free
           workshops and meetups for members of all ages and skills.
@@ -49,41 +27,53 @@ function App() {
           </a>
         </p>
       </header>
-      <p>
-        <iframe
-          title="vimeo-player"
-          src="https://player.vimeo.com/video/283072310"
-          width="640"
-          height="360"
-          frameborder="0"
-          allowfullscreen
-        ></iframe>
-      </p>
-      <p>
-        GirlsCode MK is a volunteer organisation whose aim is to get more girls
-        and women into programming and STEM careers. We’re based in Milton
-        Keynes, UK, and work together with The National Museum of Computing,
-        Open University and MK Gallery.
-      </p>
-      <p>
-        We organise regular workshops and meetups for women and provide a
-        friendly and supportive learning environment. Get in touch with us if
-        you’d like to attend as a student, supports students as a coach or
-        provide sponsorship. If you want to start your coding journey right now,
-        read this short introduction about how to become a web developer.
-      </p>
-      <p>
-        We currently organize three workshops: GirlsCode MK – a bi-weekly meetup
-        for all levels; Django Girls MK – a one-day workshop on Python and
-        basics of computer science; and NodeSchool MK, a self-paced workshop on
-        Javascript and Node.JS.
-      </p>
-      <p>
-        We are passionate about diversity and cybersecurity. If you would like
-        to learn more about staying safe online, attend CryptoParty MK – a fun
-        and welcoming event featuring workshops and talks from cybersecurity
-        experts, suitable for everyone.
-      </p>
+
+      <iframe
+        title="vimeo-player"
+        src="https://player.vimeo.com/video/283072310"
+        width="640"
+        height="360"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+      <div Blocktext>
+        <p>
+          GirlsCode MK is a volunteer organisation whose aim is to get more
+          girls and women into programming and STEM careers. We’re based in
+          Milton Keynes, UK, and work together with The National Museum of
+          Computing, Open University and MK Gallery.
+        </p>
+        <p>
+          We organise regular workshops and meetups for women and provide a
+          friendly and supportive learning environment. Get in touch with us if
+          you’d like to attend as a student, supports students as a coach or
+          provide sponsorship. If you want to start your coding journey right
+          now, read this short introduction about how to become a web developer.
+        </p>
+        <p>
+          We currently organize three workshops: GirlsCode MK – a bi-weekly
+          meetup for all levels; Django Girls MK – a one-day workshop on Python
+          and basics of computer science; and NodeSchool MK, a self-paced
+          workshop on Javascript and Node.JS.
+        </p>
+        <p>
+          We are passionate about diversity and cybersecurity. If you would like
+          to learn more about staying safe online, attend CryptoParty MK – a fun
+          and welcoming event featuring workshops and talks from cybersecurity
+          experts, suitable for everyone.
+        </p>
+        <p>
+          COVID UPDATE: We continue to meet online using zoom, where we are able
+          to invite a wider audience, pair program and share our screens in a
+          friendly, safe environment.
+        </p>
+        <p>
+          Face to Face meetups are on hold at the moment but we look forward to
+          returning to Bletchley Park where we have been supported by The
+          National Museum of Computing to encourage more women into coding.
+        </p>
+        <img src={Mansion} alt="Bletchley Park Mansion" />
+      </div>
       <p>
         Events - coming soon
         <ul>
---
+++ src/menu.js
@@ -0,0 +1,21 @@
+<div id="Menu">
+  <ul>
+    <li>
+      <a href="https://girlscodemk.herokuapp.com/">Home</a>
+    </li>
+    <li>
+      <a href="https://girlscodemk.herokuapp.com/about">About</a>
+    </li>
+    <li>
+      <a href="https://girlscodemk.herokuapp.com/blog">Blog</a>
+    </li>
+    <li>
+      <a href="https://girlscodemk.herokuapp.com/codeofconduct">
+        Code of Conduct
+      </a>
+    </li>
+    <li>
+      <a href="https://girlscodemk.herokuapp.com/contactus">Contact Us</a>
+    </li>
+  </ul>
+</div>;
